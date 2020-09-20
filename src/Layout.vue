a4d401e2383c2464cb22ed8fe1a477d0fe9c22a7
Dan6erbond <moravrav@gmail.com>
2020-09-20 16:12:27 +0200

    Fix router links

--- src/Layout.vue
+++ src/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="nav">
-      <router-link :to="{ name: 'home' }">{{ $t("home") }}</router-link>|
+      <router-link :to="{ name: 'home' }">{{ $t("home") }}</router-link> |
       <router-link :to="{ name: 'categories' }">{{ $t("categories") }}</router-link>
     </div>
     <router-view />
