9f07594da8c093c0675651d1e9dbafdb1bdc1406
Dan6erbond <moravrav@gmail.com>
2020-09-20 16:12:18 +0200

    Add striphtml filter

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -2,7 +2,22 @@ import Vue from "vue";
 
 Vue.filter("chf", function(value) {
   if (!value) return "";
-  return new Number(value).toLocaleString("de-CH", {
-    minimumFractionDigits: 2,
-  }) + " CHF";
+  return (
+    new Number(value).toLocaleString("de-CH", {
+      minimumFractionDigits: 2,
+    }) + " CHF"
+  );
+});
+
+Vue.filter("medium", function(value) {
+  if (!value) return;
+  var count = 35;
+  return value.slice(0, count) + (value.length > count ? "..." : "");
+});
+
+Vue.filter("striphtml", function(value) {
+  var div = document.createElement("div");
+  div.innerHTML = value;
+  var text = div.textContent || div.innerText || "";
+  return text;
 });
