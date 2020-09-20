d70c03bbbb41d4638920f556570e6863b9abfd71
Dan6erbond <moravrav@gmail.com>
2020-09-20 11:02:36 +0200

    Use method to fetch default languages

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,24 +1,45 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
+import Vue from "vue";
+import VueI18n from "vue-i18n";
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
-function loadLocaleMessages () {
-  const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+function loadLocaleMessages() {
+  const locales = require.context(
+    "../locales",
+    true,
+    /[A-Za-z0-9-_,\s]+\.json$/i
+  );
+  const messages = {};
+  locales.keys().forEach((key) => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = locales(key)
+      const locale = matched[1];
+      messages[locale] = locales(key);
     }
-  })
-  return messages
+  });
+  return messages;
 }
 
+function checkDefaultLanguage() {
+  let matched = null;
+  let languages = Object.getOwnPropertyNames(loadLocaleMessages());
+  languages.forEach((lang) => {
+    if (lang === navigator.language) {
+      matched = lang;
+    }
+  });
+  if (!matched) {
+    languages.forEach((lang) => {
+      let languagePartials = navigator.language.split("-")[0];
+      if (lang === languagePartials) {
+        matched = lang;
+      }
+    });
+  }
+  return matched;
+}
 export default new VueI18n({
-  locale:
-    navigator.language.split("-")[0] || process.env.VUE_APP_I18N_LOCALE || "en",
+  locale: checkDefaultLanguage() || process.env.VUE_APP_I18N_LOCALE || "en",
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
   messages: loadLocaleMessages(),
 });
